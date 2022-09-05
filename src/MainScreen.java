4c6256e4e271d57deda6dcb30c37f61b1002511c
damitzap <alex.dpinheiro@gmail.com>
2022-09-05 17:54:17 -0300

    selecao via interface grafica de aeroportos por Estado e Cidade

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -3,10 +3,9 @@ import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.*;
 
 public class MainScreen extends JFrame {
     private JLabel sourceAirpot;
@@ -17,48 +16,147 @@ public class MainScreen extends JFrame {
     private JButton btnCalculate;
     private JPanel mainPanel;
     private JTextArea lbResult;
+    private JComboBox cbState;
+    private JComboBox cbCity;
+    private JLabel lbIataSource;
+    private JComboBox cbStateDestiny;
+    private JComboBox cbCityDestiny;
+    private JLabel lbIataDestiny;
 
     public MainScreen(){
         setContentPane(mainPanel);
         setTitle("Shortest Path Between Two Airports");
-        setSize(450,300);
+        setSize(600,400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
 
-
-        //Mostrar informacoes nas comboBox de Origem e Destino
-        cbOrigin.addAncestorListener(new AncestorListener() {
-            @Override
+        //Mostrar informações comboBox selecao de estado
+        cbState.addAncestorListener(new AncestorListener() {
             public void ancestorAdded(AncestorEvent event) {
-                AirportDAO airportDAO = new AirportDAO();
-                HashMap<String,Airport> airportsList = airportDAO.getAirports();
-                cbOrigin.removeAll();
-                for (Map.Entry<String,Airport> entry : airportsList.entrySet()){
-                    cbOrigin.addItem(entry.getValue().getIata());
+                HashMap<String,Airport> airportsList = new AirportDAO().getAirports();
+                HashSet<String> stateSet = new HashSet<>();
+                for(Airport a : airportsList.values()){
+                    stateSet.add(a.getState());
+                }
+
+                for(String a : stateSet){
+                    cbState.addItem(a);
                 }
             }
-            @Override
             public void ancestorRemoved(AncestorEvent event) {}
-            @Override
             public void ancestorMoved(AncestorEvent event) {}
         });
 
-        cbDestiny.addAncestorListener(new AncestorListener() {
+        //Mostrar cidades de acordo com o estado selecionado
+        cbState.addItemListener(new ItemListener() {
             @Override
-            public void ancestorAdded(AncestorEvent event) {
-                AirportDAO airportDAO = new AirportDAO();
-                HashMap<String,Airport> airportsList = airportDAO.getAirports();
-                cbDestiny.removeAll();
-                for (Map.Entry<String,Airport> entry : airportsList.entrySet()){
-                    cbDestiny.addItem(entry.getValue().getIata());
+            public void itemStateChanged(ItemEvent e) {
+                HashMap<String, Airport> airportsList = new AirportDAO().getAirports();
+                HashSet<String> citySet = new HashSet<>();
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    cbStateDestiny.removeItem(cbState.getSelectedItem());
+                    cbCity.removeAllItems();
+                    citySet.clear();
+                    for (Airport a : airportsList.values()) {
+                        if (a.getState().equals(cbState.getSelectedItem())) {
+                            citySet.add(a.getCity());
+                        }
+                    }
+                    for(String city : citySet){
+                        cbCity.addItem(city);
+                    }
                 }
             }
-            @Override
+        });
+
+        //Mostrar Aeroporto conforme cidade escolhida
+        cbCity.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                HashMap<String, Airport> airportsList = new AirportDAO().getAirports();
+                if(e.getStateChange() == ItemEvent.SELECTED){
+                    cbOrigin.removeAllItems();
+                    for(Airport ap : airportsList.values()){
+                        if(ap.getCity().equals(cbCity.getSelectedItem())){
+                            cbOrigin.addItem(ap.getIata());
+                            lbIataSource.setText(ap.getNome());
+                        }
+                    }
+                }
+            }
+        });
+
+        //PARAMETROS PARA Aeroporto DESTINO
+        cbStateDestiny.addAncestorListener(new AncestorListener() {
+            public void ancestorAdded(AncestorEvent event) {
+                HashMap<String,Airport> airportsList = new AirportDAO().getAirports();
+                HashSet<String> stateSet = new HashSet<>();
+                for(Airport a : airportsList.values()){
+                    if(!a.getState().equals(cbState.getSelectedItem())) {
+                        stateSet.add(a.getState());
+                    }
+                }
+                for(String a : stateSet){
+                    cbStateDestiny.addItem(a);
+                }
+            }
             public void ancestorRemoved(AncestorEvent event) {}
-            @Override
             public void ancestorMoved(AncestorEvent event) {}
         });
+
+        //Mostrar cidades de acordo com o estado selecionado
+        cbStateDestiny.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                HashMap<String, Airport> airportsList = new AirportDAO().getAirports();
+                HashSet<String> citySet = new HashSet<>();
+
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    cbStateDestiny.removeItem(cbState.getSelectedItem());
+                    cbCityDestiny.removeAllItems();
+                    citySet.clear();
+                    for (Airport a : airportsList.values()) {
+                        if (a.getState().equals(cbStateDestiny.getSelectedItem())) {
+                            citySet.add(a.getCity());
+                        }
+                    }
+                    for(String city : citySet){
+                        cbCityDestiny.addItem(city);
+                    }
+                }
+            }
+        });
+
+        //Mostrar Aeroporto conforme cidade escolhida
+        cbCityDestiny.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                HashMap<String, Airport> airportsList = new AirportDAO().getAirports();
+                if(e.getStateChange() == ItemEvent.SELECTED){
+                    cbDestiny.removeAllItems();
+                    for(Airport ap : airportsList.values()){
+                        if(ap.getCity().equals(cbCityDestiny.getSelectedItem())){
+                            cbDestiny.addItem(ap.getIata());
+                            lbIataDestiny.setText(ap.getNome());
+                        }
+                    }
+                }
+            }
+        });
+//        cbDestiny.addAncestorListener(new AncestorListener() {
+//            @Override
+//            public void ancestorAdded(AncestorEvent event) {
+//                AirportDAO airportDAO = new AirportDAO();
+//                HashMap<String,Airport> airportsList = airportDAO.getAirports();
+//                cbDestiny.removeAll();
+//                for (Map.Entry<String,Airport> entry : airportsList.entrySet()){
+//                    cbDestiny.addItem(entry.getValue().getIata());
+//                }
+//            }
+//            @Override
+//            public void ancestorRemoved(AncestorEvent event) {}
+//            @Override
+//            public void ancestorMoved(AncestorEvent event) {}
+//        });
 
         //Acao realizada apos o usuario apertar o botao para Calcular a menor rota
         btnCalculate.addActionListener(new ActionListener() {
@@ -105,10 +203,19 @@ public class MainScreen extends JFrame {
                 consultDAO.save(routeSearch);
 
                 lbResult.setText("***Shortest Path Between Two Airports***"+ "\n");
-                if(graph.getNodes() != null) {
-                    lbResult.append("Source Airport: " + nodes.get(sourceAirport).getAirport().getNome() + "\n");
-                    lbResult.append("Destiny Airport: " + nodes.get(destinyAirport).getAirport().getNome()+ "\n");
-                    lbResult.append("Shortest Path: ");
+                if((graph.getNodes() != null) && !(sourceAirport.equals(destinyAirport))) {
+                    lbResult.append("Source Airport: " + nodes.get(sourceAirport).getAirport().getNome() + " - " + nodes.get(sourceAirport).getAirport().getIata()+ "\n");
+                    for(Node a : graph.getNodes()){
+                        if(a.getAirport().getIata().equals(destinyAirport)){
+                            for(Node b : a.getShortestPath()){
+                                if(b.getAirport().getIata().equals(sourceAirport)){continue;}
+                                lbResult.append("Scale: "+ b.getAirport().getNome() + " - " + b.getAirport().getIata() + "\n");
+                            }
+                        }
+
+                    }
+                    lbResult.append("Destiny Airport: " + nodes.get(destinyAirport).getAirport().getNome() + " - " + nodes.get(destinyAirport).getAirport().getIata() + "\n");
+                    lbResult.append("\nShortest Path: ");
                     for (Node node : graph.getNodes()) {
                         if (node.getAirport().getIata().equals(destinyAirport)) {
                             for (Node node1 : node.getShortestPath()) {
@@ -121,7 +228,7 @@ public class MainScreen extends JFrame {
                     lbResult.append(" " + nodes.get(destinyAirport).getAirport().getIata());
 
                 }else{
-                    lbResult.append("Unknown Path");
+                    lbResult.setText("Unknown Path");
                 }
 
             }
@@ -134,6 +241,7 @@ public class MainScreen extends JFrame {
                 lbResult.setText("");
             }
         });
+
     }
 
     public static void main(String[] args) {
